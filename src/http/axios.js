c6d54e67a30cdfc558f9a6b08bcede8d41f90ba6
Khrystyna <hrystynka.dm@gmail.com>
2023-03-07 15:35:02 +0200

    fff

---
+++ src/http/axios.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const SERVER_URL = "https://insta-0u51.onrender.com"
+//const SERVER_URL= "http://localhost:4000"
+
+
+export const api = axios.create({
+    withCredentials: true,
+    baseURL: SERVER_URL,
+});
+
+api.interceptors.request.use((req) => {
+    req.headers.Authorization = `Bearer ${localStorage.getItem("fb_token")}`;
+    return req;
+});
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,9 @@
-export function User(){
-    return <div>
-        
+
+import { api } from "../http/axios"
+
+export function User() {
+
+    return <div onClick={async () => await api.get('/')}>
+        gggg
     </div>
 }
